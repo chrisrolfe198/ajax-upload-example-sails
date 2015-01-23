514f3809c4357e249f999e8a4ce001300db8d9ed
Christopher Rolfe <christopher.rolfe198@gmail.com>
2015-01-23 18:56:47 +0000

    Changing the file extension

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -12,7 +12,7 @@ module.exports = {
 	ajax_upload: function(req, res) {
 		req.file('image').upload({
 			dirname: 'widget-bg/',
-			saveAs: 'test.jpeg'
+			saveAs: 'test.jpg'
 		}, function(err, uploaded) {
 			console.log(err);
 			console.log(uploaded);
